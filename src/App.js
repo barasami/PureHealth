9529114d8a64434e55af5677dc0fa0a9877e21a9
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-05-04 21:25:43 +0300

    added components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import Homeabout from './About/Homeabout';
 import Homeexercise from './Exercise/Homeexercise';
 import Homefit from './FitnescALc/Homefit';
 import Homecalorie from './Calories/Homecalorie';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import { Fragment } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <Homeabout/>
-      <Homeexercise/>
-      <Homefit/>
-      <Homecalorie/>
-    </div>
+    <Router>
+      <Fragment>
+        <Routes>
+          <Homeabout/>
+          <Homeexercise/>
+          <Homefit/>
+          <Homecalorie/>
+        </Routes>
+      </Fragment>
+    </Router>
   );
 }
 
