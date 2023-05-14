7b290a56b37e64d8197f9ec7dc2046e3f4ac2ed0
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-05-14 21:09:47 +0300

    modified pages

--- src/Exercise/Homeexercise.js
+++ src/Exercise/Homeexercise.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { myExercise } from './exercise'
-import { CircularProgress } from '@mui/material'
+import { Box, CircularProgress, Container, Grid, Paper, Typography } from '@mui/material'
 import './Exercise.css'
 
 function Homeexercise() {
@@ -20,18 +20,26 @@ function Homeexercise() {
     let myData=exercise?.map((abj)=>{
       const{bodypart,equipment,id,name,target,gifUrl}=abj
       return(
-        <div key={id}>
-          <div>
-            <div>{name}</div>
-            <div>{target}</div>
-            <div>{bodypart}</div>
-            <div>{equipment}</div>
-            <div>
+        <Grid key={id} item lg={4} sm={8} xs={12}>
+          <Grid elevation={10}>
+            <Typography variant='h6' color='seagreen'>
+              {name}
+            </Typography>
+            <Typography variant='h8' color='green'>
+              Target {target}
+            </Typography>
+            <Typography variant='h8' color='green'>
+              Body part{bodypart}
+            </Typography>
+            <Typography color='green'>
+              Equipment {equipment}
+            </Typography>
+            <Box>
               <img src={gifUrl} alt={name}/>
-            </div>
-          </div>
+            </Box>
+          </Grid >
   
-        </div>
+        </Grid>
       )
     })
 
@@ -40,9 +48,11 @@ function Homeexercise() {
     <div className='coolexercise'>
       <div className='exercisedata'>
         {load ? <CircularProgress color='inherit' className='circular'/> : 
-        <div className='exercisefinal'> 
-          {myData}
-        </div>}
+        <Container className='exercisefinal'> 
+          <Grid container spacing={3}>
+            {myData}
+          </Grid>
+        </Container>}
       </div>
     </div>
   )
